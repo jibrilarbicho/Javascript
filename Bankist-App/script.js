e90f506521a88a19d1e444aa68b5356869114ee7
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-10-07 17:39:09 +0300

    Implementing Delete Account

--- Bankist-App/script.js
+++ Bankist-App/script.js
@@ -190,3 +190,17 @@ btnTransfer.addEventListener("click", function (e) {
   }
   inputTransferAmount.value = inputTransferTo.value = "";
 });
+btnClose.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      (acc) => acc.username === currentAccount.username
+    );
+    accounts.splice(index, 1);
+    //Hide UI
+    containerApp.style.opacity = 0;
+  }
+});
