52e81015979bf1fdc0c533a8ff8238a374fb5cfb
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-10-10 00:44:17 +0300

    Tabbed components

--- Bankist-Website/script.js
+++ Bankist-Website/script.js
@@ -149,3 +149,21 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
+//DOM Traversing
+h1.firstElementChild.style.color = "white";
+h1.lastElementChild.style.color = "orangered";
+const tabs = document.querySelectorAll(".operations__tab");
+const tabContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+tabContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+  console.log(clicked);
+  if (!clicked) return;
+  tabsContent.forEach((k) => k.classList.remove("operations__content--active"));
+  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+  clicked.classList.add("operations__tab--active");
+  console.log(clicked.dataset.tab);
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
