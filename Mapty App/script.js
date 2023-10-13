0991172f7099b7c19ebf15969706718d41464d88
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-10-13 21:31:04 +0300

    rendering input form

--- Mapty App/script.js
+++ Mapty App/script.js
@@ -4,9 +4,54 @@
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 const form = document.querySelector(".form");
+
 const containerWorkouts = document.querySelector(".workouts");
 const inputType = document.querySelector(".form__input--type");
 const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+let map, mapEvent;
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      const coords = [latitude, longitude];
+      console.log(position);
+      map = L.map("map").setView(coords, 13);
+
+      L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      //the folllowing on method is found only in lefalet Library
+      map.on("click", function (mapE) {
+        mapEvent = mapE;
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+        form.classList.remove("hidden");
+        inputDistance.focus();
+      });
+    },
+    function () {
+      alert("Could not get your position");
+    }
+  );
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        clasName: "running-popup",
+      })
+    )
+    .setPopupContent("workout")
+    .openPopup();
+});
